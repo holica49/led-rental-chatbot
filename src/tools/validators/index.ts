5505f47e0311d9a6b8cf0058cf56a38fb13a8e90
holica49 <kgj205@gmail.com>
2025-07-25 12:31:47 +0900

    개선된 챗봇 : 코드 로직 분기

--- src/tools/validators/index.ts
+++ src/tools/validators/index.ts
@@ -1,12 +1,5 @@
-// src/tools/validators/index.ts
-export * from './led-validator.js';
-export * from './stage-height-validator.js';
-export * from './phone-validator.js';
-export * from './date-validator.js';
-export * from './common-validator.js';
-
-export { validateAndNormalizeLEDSize } from './led-validator';
-export { validatePhoneNumber } from './phone-validator';
-export { validateEventPeriod } from './date-validator';
-export { validateStageHeight } from './stage-height-validator';
-export { validateNumber } from './common-validator';
+export { validateAndNormalizeLEDSize } from './led-validator.js';
+export { validatePhoneNumber } from './phone-validator.js';
+export { validateEventPeriod } from './date-validator.js';
+export { validateStageHeight } from './stage-height-validator.js';
+export { validateNumber } from './common-validator.js';
